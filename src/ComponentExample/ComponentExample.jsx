3157be1267d59b2096ce117a0f7c94921be871a6
ardygh <rdghoorchian@gmail.com>
2024-01-13 13:19:07 -0800

    [Ardy]: starter code added

---
+++ src/ComponentExample/ComponentExample.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './zappplyLogo.png';
+
+function ComponentExample(props) {
+    return (
+        <img
+            src={logo}
+            alt={"zappply ai logo"}
+        />
+    );
+}
+
+export default ComponentExample;
---
+++ src/ComponentExample/ComponentExample.stories.jsx
@@ -0,0 +1,14 @@
+import ComponentExample from './ComponentExample';
+
+export default {
+    title: 'Example/ComponentExample',
+    component: ComponentExample,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+};
+
+export const Default = {
+    args: {},
+};
